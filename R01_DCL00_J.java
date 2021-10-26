1ae57aedd79b8d08c289e4ee7d56dedf6afb78ab
RCuriel1215 <70348012+RCuriel1215@users.noreply.github.com>
2021-10-25 21:28:30 -0400

    Non-compliant code

--- R01_DCL00_J.java
+++ R01_DCL00_J.java
@@ -1,6 +1,6 @@
 
 public class R01_DCL00_J {
-
+ 
 		  private final int balance;
 		  private static final R01_DCL00_J c = new R01_DCL00_J();
 		  private static final int deposit = (int) (Math.random() * 100); // Random deposit
