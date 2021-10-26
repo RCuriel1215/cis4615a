b53a825acb848f5ffd3eebc2c3a5e61481c61776
RCuriel1215 <70348012+RCuriel1215@users.noreply.github.com>
2021-10-25 21:10:40 -0400

    Non-compliant code

---
+++ R07_ERR02_J.java
@@ -0,0 +1,11 @@
+
+public class R07_ERR02_J {
+
+try {
+  // ...
+} catch (SecurityException se) {
+  System.err.println(se);
+  // Recover from exception
+}
+	
+}
---
+++ R09_LCK01_J.java
@@ -0,0 +1,12 @@
+
+public class R09_LCK01_J {
+	
+	private final Boolean initialized = Boolean.FALSE;
+	 
+	public void doSomething() {
+	  synchronized (initialized) {
+	    // ...
+	  }
+	}
+
+}
